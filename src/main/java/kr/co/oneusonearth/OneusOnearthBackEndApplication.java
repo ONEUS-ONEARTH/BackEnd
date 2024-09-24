d4a75e20518bca75c7eeadbfe74029347a093b07
seon <darkness.blooms.sun@gmail.com>
2024-09-24 15:30:26 +0900

    init

---
+++ src/main/java/kr/co/oneusonearth/OneusOnearthBackEndApplication.java
@@ -0,0 +1,13 @@
+package kr.co.oneusonearth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneusOnearthBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneusOnearthBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/oneusonearth/OneusOnearthBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.oneusonearth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneusOnearthBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
