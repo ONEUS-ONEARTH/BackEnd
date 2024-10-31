6c9627951aefddaeee7f4b3255d747a3e554d241
seon <sunkyoung.dev@gmail.com>
2024-11-01 01:14:39 +0900

    CrosConfig

---
+++ src/main/java/kr/co/ouoe/common/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package kr.co.ouoe.common.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")  // 모든 경로에 CORS 설정
+                    .allowedOrigins("http://localhost:3000","http://localhost:8080")  // 허용할 도메인
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
+                    .allowedHeaders("*")  // 허용할 헤더
+                    .allowCredentials(true)  // 쿠키 등 인증정보 허용 여부
+                    .maxAge(3600);  // pre-flight 요청 캐싱 시간 (초 단위)
+        }
+
+
+
+}
