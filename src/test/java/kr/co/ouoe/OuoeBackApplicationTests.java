f2c60b5922ee5c949e0a133ccec156a2afc7a146
seon <sunkyoung.dev@gmail.com>
2024-11-01 13:44:05 +0900

    기본 파일

---
+++ src/test/java/kr/co/ouoe/OuoeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ouoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuoeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
