f7343150ad6bc2b08d7e4fcd8aca3fa0fe579c3c
seon <sunkyoung.dev@gmail.com>
2024-11-05 14:14:38 +0900

    안쓰는 메서드 정리

--- src/main/java/kr/co/ouoe/EcoStore/repository/EcoStoreReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.co.ouoe.EcoStore.repository;
-
-import kr.co.ouoe.EcoStore.domain.EcoStore;
-import kr.co.ouoe.EcoStore.domain.EcoStoreReviewPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EcoStoreReviewRepository extends JpaRepository<EcoStoreReviewPost,Long> {
-}
--- src/main/java/kr/co/ouoe/EcoStore/repository/EcoStorerepository.java
+++
@@ -1,7 +0,0 @@
-package kr.co.ouoe.EcoStore.repository;
-
-import kr.co.ouoe.EcoStore.domain.EcoStore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EcoStorerepository extends JpaRepository<EcoStore,Long> {
-}
