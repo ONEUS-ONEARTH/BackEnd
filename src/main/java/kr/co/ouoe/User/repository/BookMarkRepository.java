0329c7bd70f5e7af0db84fed245b6894cc45849d
seon <sunkyoung.dev@gmail.com>
2024-11-09 13:51:53 +0900

    북마크 관련 레파지토리 메서드 추가

--- src/main/java/kr/co/ouoe/User/repository/BookMarkRepository.java
+++ src/main/java/kr/co/ouoe/User/repository/BookMarkRepository.java
@@ -1,8 +1,9 @@
 package kr.co.ouoe.User.repository;
 
 import kr.co.ouoe.User.entity.BookMark;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookMarkRepository extends JpaRepository<BookMark, Long> {
-
+   boolean existsBookMarkByUserIdAndAndPostId(long userId, long postId);
 }
