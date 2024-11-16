1357db22a1120b3ba82008c0a3cf523e9a573e90
seon <sunkyoung.dev@gmail.com>
2024-11-17 01:41:46 +0900

    모임지도 JPARepositiroes 수정

--- src/main/java/kr/co/ouoe/MeetingPost/repository/MeetingLocateRepository.java
+++ src/main/java/kr/co/ouoe/MeetingPost/repository/MeetingLocateRepository.java
@@ -1,9 +1,7 @@
 package kr.co.ouoe.MeetingPost.repository;
 
-import kr.co.ouoe.MeetingPost.domain.MeetingLocate;
+import kr.co.ouoe.MeetingPost.entity.MeetingLocate;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.NoRepositoryBean;
 
 
 public interface MeetingLocateRepository extends JpaRepository<MeetingLocate,Long> {
