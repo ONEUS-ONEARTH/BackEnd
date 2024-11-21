45873b06783e9b83d568ceb3720ad770a049a18a
seon <sunkyoung.dev@gmail.com>
2024-11-21 16:58:53 +0900

    모임페이지에서 불러오기 성공 레파지 토리 메서드 추가

--- src/main/java/kr/co/ouoe/MeetingPost/repository/MeetingLocateRepository.java
+++ src/main/java/kr/co/ouoe/MeetingPost/repository/MeetingLocateRepository.java
@@ -3,6 +3,10 @@ package kr.co.ouoe.MeetingPost.repository;
 import kr.co.ouoe.MeetingPost.entity.MeetingLocate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface MeetingLocateRepository extends JpaRepository<MeetingLocate,Long> {
+    @Override
+    List<MeetingLocate> findAll();
 }
