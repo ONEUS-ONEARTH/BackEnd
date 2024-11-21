cf30e4eee78232ccd267f3e1babd04ce3c8500ac
seon <sunkyoung.dev@gmail.com>
2024-11-21 21:06:34 +0900

    cros config 수정

--- src/main/java/kr/co/ouoe/common/config/CorsConfig.java
+++ src/main/java/kr/co/ouoe/common/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")  // 모든 경로에 CORS 설정
-                    .allowedOrigins("http://localhost:3000","http://localhost:8080")  // 허용할 도메인
+                    .allowedOrigins("http://localhost:3000","http://localhost:8080","http://ouoereact.s3-website.ap-northeast-2.amazonaws.com/")  // 허용할 도메인
                     .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
                     .allowedHeaders("*")  // 허용할 헤더
                     .allowCredentials(true)  // 쿠키 등 인증정보 허용 여부
